2711e5ae1776ea9267a4fec572d2e829f71591fd
jorge <alarcj137@gmail.com>
2016-01-30 20:23:36 -0500

    no hard coding on combine2gro

--- op_run/combine2gro.sh
+++ op_run/combine2gro.sh
@@ -5,9 +5,17 @@ file2=$2
 output_file=$3
 head --lines=-1 $file1 > $output_file
 tail --lines=+3 $file2 >> $output_file
+
+# Total number of atoms
 tot=$((`wc -l $output_file | awk '{print $1}'` -3));
+# Replace number of atoms
 sed -i "2c\ $tot" $output_file
-sed -i "`echo "${tot} + 3" | bc -l`s/.*/   3.90000   3.90000   7.80000/" $output_file
+# Get dimensions of box
+$boxsize=`awk 'NR=='$tot+3' {print $1}' $output_file`
+# One the size of the box in the z axis should be twice as large
+boxz=`echo "2 * $boxsize" | bc -l`
+# Replace sizes where appropiate  (sed -i "`echo "${tot} + 3" | bc -l`s/.*/   3.90000   3.90000   7.80000/" $output_file)
+sed -i "`echo "${tot} + 3" | bc -l`s/.*/   $boxsize   $boxsize   $boxz/" $output_file
 
 OW=$(grep OW $output_file |wc -l)
 #var=`tail -1 topol.top`
